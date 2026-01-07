10ed2da5803a6d8c4c8fd9513214f3ed16a793b2
Zamanov Nadir Rasim <Zamanov@itstep.edu.az>
2026-01-07 16:21:59 +0400

    Mini ASP

---
+++ ASP NET 02. Mini ASP/Middlewares/StaticFilesMiddleware.cs
@@ -0,0 +1,41 @@
+﻿using ASP_NET_02._Mini_ASP.Interfaces;
+using System.Net;
+
+namespace ASP_NET_02._Mini_ASP.Middlewares;
+
+class StaticFilesMiddleware : IMiddleware
+{
+    public HttpHandler Next { get; set; }
+
+    public void Handle(HttpListenerContext context)
+    {
+       if (Path.HasExtension(context.Request.RawUrl))
+        {
+            try
+            {
+                var fileName = context.Request.RawUrl.Substring(1);
+                var path = $@"..\..\..\wwroot\{fileName}";
+                var bytes = File.ReadAllBytes(path);
+                if (Path.GetExtension(path) == ".html")
+                {
+                    context.Response.AddHeader("Content-Type", "text/html");
+                }
+                else if(Path.GetExtension(path) == ".png")
+                {
+                    context.Response.AddHeader("Content-Type", "image/png");
+                }
+                else
+                {
+                    Next.Invoke(context);
+                }
+                    context.Response.OutputStream.Write(bytes, 0, bytes.Length);
+            }
+            catch (Exception)
+            {
+                context.Response.StatusCode = 404;
+                context.Response.StatusDescription = "File Not Found";
+            }
+        }
+        context.Response.Close();
+    }
+}
