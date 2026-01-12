a7bf7dc07627fad320a689c6945527dfd59fe5d1
Zamanov Nadir Rasim <Zamanov@itstep.edu.az>
2026-01-12 17:00:16 +0400

    MVC

---
+++ ASP NET 04. Products MVC/Data/ProductsMVCContext.cs
@@ -0,0 +1,13 @@
+﻿using ASP_NET_04._Products_MVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASP_NET_04._Products_MVC.Data;
+
+public class ProductsMVCContext : DbContext
+{
+    public ProductsMVCContext(DbContextOptions options) 
+        : base(options)
+    {}
+
+    public DbSet<Product> Products => Set<Product>();
+}
