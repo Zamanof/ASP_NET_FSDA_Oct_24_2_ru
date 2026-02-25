ebc3f99c06d44b903ef5fd2672460959eec73d59
Zamanof <zamanov@itstep.org>
2026-02-25 23:36:58 +0400

    CQRS

---
+++ ASP NET 22. TaskFlow CQRS.Api/Program.cs
@@ -0,0 +1,25 @@
+using FluentValidation.AspNetCore;
+using ASP_NET_22._TaskFlow_CQRS.Api.Extensions;
+using ASP_NET_22._TaskFlow_CQRS.Application.Extensions;
+using ASP_NET_22._TaskFlow_CQRS.Infrastructure.Extensions;
+
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddApplication()
+    .AddInfrastructure(builder.Configuration)
+    .AddIdentityAndJwt(builder.Configuration);
+
+builder.Services.AddSwagger();
+builder.Services.AddJwtAuthenticationAndAuthorization(builder.Configuration);
+builder.Services.AddCorsPolicy();
+builder.Services.AddFluentValidationAutoValidation();
+
+var app = builder.Build();
+
+app.UseTaskFlowPipeline();
+await app.EnsureRolesSeededAsync();
+
+app.Run();
---
+++ ASP NET 22. TaskFlow CQRS.Application/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using  ASP_NET_22._TaskFlow_CQRS.Application.Mapping;
+using  ASP_NET_22._TaskFlow_CQRS.Application.Services;
+using  ASP_NET_22._TaskFlow_CQRS.Application.Validators;
+
+namespace  ASP_NET_22._TaskFlow_CQRS.Application.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddValidatorsFromAssemblyContaining<RegisterValidator>();
+        services.AddAutoMapper(typeof(MappingProfile));
+        services.AddScoped<IProjectService, ProjectService>();
+        services.AddScoped<ITaskItemService, TaskItemService>();
+        services.AddScoped<IAuthService, AuthService>();
+        services.AddScoped<IAttachmentService, AttachmentService>();
+        return services;
+    }
+}
